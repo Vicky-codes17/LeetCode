11aef7c493c5dc599f52c6c300e61fcfab2599f4
Vicky-codes17 <rameshvignesh0706@gmail.com>
2026-02-03 15:53:49 +0530

    Added: Reverse_bits-190-LEETCODE

---
+++ Python/Easy/Reverse_bits-190/reverse_bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse_bit(self,n):
+        c = 0
+        for i in range(32):
+            val = n&1
+            c = (c<<i) | val
+            n = n >> 1
+        return c
+# print(Solution().reverse_bit(43261596))
+      
+            
+ 
