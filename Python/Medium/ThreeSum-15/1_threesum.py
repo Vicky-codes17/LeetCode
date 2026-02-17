2eba691c88f4bcf237c522ddd01b4b05d94ac411
Vignesh R <rameshvignesh0706@gmail.com>
2026-02-17 10:37:25 +0530

    Added: threesum.py

---
+++ Python/Medium/ThreeSum-15/1_threesum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        res = []
+        for i in range(len(nums)-2):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            l  = i + 1
+            r = len(nums) - 1
+            while l<r:
+                s = nums[i] + nums[l] + nums[r]
+                if s<0:
+                    l+=1
+                elif s>0:
+                    r -=1
+                else:
+                    res.append((nums[i],nums[l],nums[r]))
+                    l += 1
+                    r -= 1
+        res = set(res)
+        return list(res)
+print(Solution().threeSum([-1,0,1,2,-1,-4]))
+       
